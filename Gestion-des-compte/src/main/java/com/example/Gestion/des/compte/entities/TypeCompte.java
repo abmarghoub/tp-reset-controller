542dc233219855bddb5c41330277d9fdae001bd1
abmarghoub <a.marghoub5257@uca.ac.ma>
2025-11-01 03:16:43 +0100

    Gestion des comptes

---
+++ Gestion-des-compte/src/main/java/com/example/Gestion/des/compte/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.Gestion.des.compte.entities;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ Gestion-des-compte/src/test/java/com/example/Gestion/des/compte/GestionDesCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.des.compte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesCompteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
